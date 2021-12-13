d972512c43f45b1808f5db8430efbb56b98e2352
AlexSD92 <alex.spara.dominguez@gmail.com>
2021-12-13 21:21:49 +0000

    added startbtn class to start button and assigned it to variable start in index.js

--- assets/index.js
+++ assets/index.js
@@ -5,3 +5,4 @@ let one = document.getElementById("btn1");
 let two = document.getElementById("btn2");
 let three = document.getElementById("btn3");
 let four = document.getElementById("btn4");
+let start = document.getElementById("startbtn");
