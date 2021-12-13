195eb41e69f1e151c0f6180f52ac3552c41feec5
AlexSD92 <alex.spara.dominguez@gmail.com>
2021-12-13 21:18:40 +0000

    assigned btns1-4 to variables one, two, three, and four

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,7 @@
 userChoice = [];
 gameChoice = [];
 choices = [1,2,3,4];
+let one = document.getElementById("btn1");
+let two = document.getElementById("btn2");
+let three = document.getElementById("btn3");
+let four = document.getElementById("btn4");
