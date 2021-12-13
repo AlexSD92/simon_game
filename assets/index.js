5e979cb465fa6014907821e0611e44858d01013f
AlexSD92 <alex.spara.dominguez@gmail.com>
2021-12-13 21:29:34 +0000

    added functions for each btn1-4 that pushed a value in to the userChoice array; all triggered by onclick which call the function

--- assets/index.js
+++ assets/index.js
@@ -6,3 +6,28 @@ let two = document.getElementById("btn2");
 let three = document.getElementById("btn3");
 let four = document.getElementById("btn4");
 let start = document.getElementById("startbtn");
+
+function clickOne(){
+  userChoice.push(1);
+  console.log(1);
+}
+
+function clickTwo(){
+  userChoice.push(2);
+  console.log(2);
+}
+
+function clickThree(){
+  userChoice.push(3);
+  console.log(3);
+}
+
+function clickFour(){
+  userChoice.push(4)
+  console.log(4);
+}
+
+one.onclick = clickOne;
+two.onclick = clickTwo;
+three.onclick = clickThree;
+four.onclick = clickFour;
