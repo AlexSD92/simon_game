095dd48d751e6c88fe3796033cff9123f10ae196
AlexSD92 <alex.spara.dominguez@gmail.com>
2021-12-15 21:13:03 +0000

    created waituserChoice() to listen for user clicking of simon buttons; created testChoice() which tests the validity of the userChoice[] every time the user clicks a simon button

--- assets/index.js
+++ assets/index.js
@@ -1,37 +1,67 @@
 userChoice = [];
 gameChoice = [];
 choices = [1,2,3,4];
+// xxx = true;
 let one = document.getElementById("btn1");
 let two = document.getElementById("btn2");
 let three = document.getElementById("btn3");
 let four = document.getElementById("btn4");
 let start = document.getElementById("startbtn");
 
-function clickOne(){
-  userChoice.push(1);
-  console.log(1);
-}
-
-function clickTwo(){
-  userChoice.push(2);
-  console.log(2);
-}
-
-function clickThree(){
-  userChoice.push(3);
-  console.log(3);
-}
-
-function clickFour(){
-  userChoice.push(4)
-  console.log(4);
-}
 
 function randomSequence(){
   gameChoice.push(Math.floor((Math.random() * 4) + 1));
 }
 
+function testChoice(){
+  for(i=0; i<userChoice.length; i++){
+    if(userChoice[i] === gameChoice[i] && userChoice.length < gameChoice.length){
+      console.log("win");
+    }
+    else if(userChoice[i] === gameChoice[i] && userChoice.length === gameChoice.length){
+      userChoice = [];
+      playGameChoice();
+    }
+    else{console.log("You Lose")}
+  }
+}
+
+function waitUserChoice(){
+
+  function clickOne(){
+    userChoice.push(1);
+    console.log(1);
+    testChoice();
+  }
+
+  function clickTwo(){
+    userChoice.push(2);
+    console.log(2);
+    testChoice();
+  }
+
+  function clickThree(){
+    userChoice.push(3);
+    console.log(3);
+    testChoice();
+  }
+
+  function clickFour(){
+    userChoice.push(4)
+    console.log(4);
+    testChoice();
+  }
+
+  one.onclick = clickOne;
+  two.onclick = clickTwo;
+  three.onclick = clickThree;
+  four.onclick = clickFour;
+
+}
+
 function playGameChoice(){
+
+  randomSequence();
 
   for (i=0; i<gameChoice.length; i++){
 
@@ -69,16 +99,12 @@ function playGameChoice(){
 
   }
 
+  waitUserChoice();
+
 }
 
 function startGame(){
-  randomSequence();
   playGameChoice();
 }
-
-one.onclick = clickOne;
-two.onclick = clickTwo;
-three.onclick = clickThree;
-four.onclick = clickFour;
 
 start.onclick = startGame;
