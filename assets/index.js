e8e67fb3aa3a333c6763746111e93a24eb3c5604
AlexSD92 <alex.spara.dominguez@gmail.com>
2021-12-20 21:20:47 +0000

    added event listeners for ctrl buttons, added level to update on each round, added hidden class to appropriate objects

--- assets/index.js
+++ assets/index.js
@@ -9,15 +9,44 @@ let two = document.getElementById("btn2");
 let three = document.getElementById("btn3");
 let four = document.getElementById("btn4");
 let start = document.getElementById("startbtn");
+let reset = document.getElementById("resetbtn");
 let btn1Sound = new Audio("assets/sounds/btn1Sound.mp3");
 let btn2Sound = new Audio("assets/sounds/btn2Sound.mp3");
 let btn3Sound = new Audio("assets/sounds/btn3Sound.mp3");
 let btn4Sound = new Audio("assets/sounds/btn4Sound.mp3");
+let btnWrapper = document.getElementById("btnwrapper");
 let gameOverSound = new Audio("assets/sounds/gameOverSound.mp3");
 let gameOverMsg = document.getElementById("gameover");
+let levelHeader = document.getElementById("levelheader");
+let levelNumber = document.getElementById("levelnumber");
 
 function randomSequence(){ // generates random number between 1 and 4 and pushes it to gameChoice array
   gameChoice.push(Math.floor((Math.random() * 4) + 1));
+}
+
+function animate(button){ // created for consistent code and animaation, used specifically for user clicks of simon buttons
+  setTimeout(() => {
+    button.classList.add("clicked");
+    setTimeout(() => {button.classList.remove("clicked")}, 200);
+  }, 100);
+}
+
+function startClicked(){
+  animate(start);
+  startGame();
+}
+
+function resetClicked(){
+  animate(reset);
+  userChoice = [];
+  gameChoice = [];
+  start.classList.remove("hidden");
+  reset.classList.add("hidden");
+  one.classList.remove("hidden");
+  two.classList.remove("hidden");
+  three.classList.remove("hidden");
+  four.classList.remove("hidden");
+  gameOverMsg.classList.add("hidden");
 }
 
 function gameOver(){ // ends the game, activated if user makes an incorrect choice; empties both userChoice and gameChoice arrays
@@ -26,8 +55,8 @@ function gameOver(){ // ends the game, activated if user makes an incorrect choi
   two.classList.add("hidden");
   three.classList.add("hidden");
   four.classList.add("hidden");
-  // userChoice = [];
-  // gameChoice = [];
+  reset.classList.remove("hidden");
+
   gameOverSound.play();
 }
 
@@ -51,16 +80,60 @@ function testChoice(){ // tests array equality by iteration
         else if(userChoice[i] === gameChoice[i]){
             console.log("next round");
             userChoice = [];
-            setTimeout(()=>{return playGameChoice();},2000);
+            level++;
+            setTimeout(playGameChoice, 2000);
           }
       }
   }
 }
 
 
+function waitUserChoice(){
+
+    function clickOne(){
+      userChoice.push(1);
+      console.log(1);
+      animate(one)
+      btn1Sound.play();
+      testChoice();
+    }
+
+    function clickTwo(){
+      userChoice.push(2);
+      console.log(2);
+      animate(two);
+      btn2Sound.play();
+      testChoice();
+    }
+
+    function clickThree(){
+      userChoice.push(3);
+      console.log(3);
+      animate(three);
+      btn3Sound.play();
+      testChoice();
+    }
+
+    function clickFour(){
+      userChoice.push(4)
+      console.log(4);
+      animate(four);
+      btn4Sound.play();
+      testChoice();
+    }
+
+    one.onclick = clickOne;
+    two.onclick = clickTwo;
+    three.onclick = clickThree;
+    four.onclick = clickFour;
+
+  }
+
 function playGameChoice(){ // plays the games turn, animations in place for each gameChoice made, calls waitUserChoice at the end
 
   randomSequence();
+  levelNumber.innerHTML = level;
+
 
   for (i=0; i<gameChoice.length; i++){
 
@@ -102,53 +175,6 @@ function playGameChoice(){ // plays the games turn, animations in place for each
 
   }
 
-  function waitUserChoice(){
-
-    function animate(button){ // created for consistent code and animaation, used specifically for user clicks of simon buttons
-      setTimeout(() => {
-        button.classList.add("clicked");
-        setTimeout(() => {button.classList.remove("clicked")}, 200);
-      }, 100);
-    }
-
-    function clickOne(){
-      userChoice.push(1);
-      console.log(1);
-      animate(one)
-      btn1Sound.play();
-      testChoice();
-    }
-
-    function clickTwo(){
-      userChoice.push(2);
-      console.log(2);
-      animate(two);
-      btn2Sound.play();
-      testChoice();
-    }
-
-    function clickThree(){
-      userChoice.push(3);
-      console.log(3);
-      animate(three);
-      btn3Sound.play();
-      testChoice();
-    }
-
-    function clickFour(){
-      userChoice.push(4)
-      console.log(4);
-      animate(four);
-      btn4Sound.play();
-      testChoice();
-    }
-
-    one.onclick = clickOne;
-    two.onclick = clickTwo;
-    three.onclick = clickThree;
-    four.onclick = clickFour;
-
-  }
 
   waitUserChoice();
 
@@ -156,9 +182,13 @@ function playGameChoice(){ // plays the games turn, animations in place for each
 
 
 function startGame(){ // tied to start.onclick, initiates playGameChoice() after 1 second
+  start.classList.add("hidden");
+  levelHeader.classList.remove("hidden");
+  levelNumber.innerHTML = level
   setTimeout(() => {
     playGameChoice();
   }, 1000)
 }
 
-start.onclick = startGame; // is this appropriate?
+reset.onclick = resetClicked;
+start.onclick = startClicked;
