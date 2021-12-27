ae9f579cb0119da64c9ca67da5a1fff7235118c7
Alex Dominguez <alex.spara.dominguez@gmail.com>
2021-12-27 06:28:31 +0000

    styled the ol, adjusted the javascript for better readability

--- assets/high_scores.js
+++ assets/high_scores.js
@@ -2,5 +2,6 @@ const highScores = JSON.parse(localStorage.getItem("scoreList")) ?? [];
 const highScoreList = document.getElementById("highscores");
 
 highScoreList.innerHTML = highScores
-  .map((scoreList) => `<li>${scoreList.level} - ${scoreList.userName}`)
+  .map((scoreList) => `<li>${scoreList.userName} - - - ${scoreList.level}`)
+  // .map((scoreList) => '<tr>')
   .join('');
