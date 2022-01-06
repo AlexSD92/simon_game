7ca53e6ba86c7fe751d5e7d7f8152afb940cb15b
Alex Dominguez <alex.spara.dominguez@gmail.com>
2022-01-06 20:45:20 +0000

    removed all commented out code; added commented secitons explaining functions; added document event listener to prevent buttons and forms from being submitted by pressing enter

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,3 @@
-// https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
 /*jshint esversion: 11 */
 let userChoice = [];
 let gameChoice = [];
@@ -15,7 +14,6 @@ let btn1Sound = new Audio("assets/sounds/btn1Sound.mp3");
 let btn2Sound = new Audio("assets/sounds/btn2Sound.mp3");
 let btn3Sound = new Audio("assets/sounds/btn3Sound.mp3");
 let btn4Sound = new Audio("assets/sounds/btn4Sound.mp3");
-// let btnWrapper = document.getElementById("btnwrapper");
 let gameOverSound = new Audio("assets/sounds/gameoverSound.mp3");
 let gameOverMsg = document.getElementById("gameover");
 let levelHeader = document.getElementById("levelheader");
@@ -24,18 +22,16 @@ let submit = document.getElementById("submitname");
 let submittedMsg = document.getElementById("scoresubmitted");
 let form = document.getElementById("enterdetails");
 let name = document.getElementById("namefield");
-// const NO_OF_HIGH_SCORES = 10;
-// const HIGH_SCORES = 'highScores';
-// const highScoreString = localStorage.getItem(HIGH_SCORES);
-// const highScores = JSON.parse(highScoreString) ?? [];
-
-//const scoreList = JSON.parse(localStorage.getItem("scoreList"));
-// https://stackoverflow.com/questions/43762363/how-to-store-an-array-of-objects-in-local-storage
-
 const no_of_scores = 3;
 
-// ↓↓↓ https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68=
+// https://stackoverflow.com/questions/43762363/how-to-store-an-array-of-objects-in-local-storage
+
+
+// https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68=
 // https://stackoverflow.com/questions/17087636/how-to-save-data-from-a-form-with-html5-local-storage
+
+/* checks whether the score achieved is greater than the lowest score in localstorage, if it is, the form is displayed to take the score, on click pushScore is activated
+pushScore takes the name from the form field, the score achieved and pushes it in to scoreList locally */
 
 function checkScores(){
 
@@ -44,20 +40,11 @@ function checkScores(){
     levelHeader.classList.add("hidden");
     submittedMsg.classList.remove("hidden");
     form.classList.add("hidden");
-    // const userName = prompt("You got a new score! Enter your name:");
     let userName = name.value;
-    // JSON.stringify(userName);
-    // storeName();
-
-    // const userName = localStorage.setItem("userName", value.value);
     const newScore = {level, userName};
-
     scoreList.push(newScore);
-
     scoreList.sort((a, b) => b.level - a.level);
-
     scoreList.splice(no_of_scores);
-
     localStorage.setItem("scoreList", JSON.stringify(scoreList));
 
   }
@@ -68,7 +55,7 @@ function checkScores(){
   if (level > lowestScore){
     form.classList.remove("hidden");
     // pushScore();
-    submit.addEventListener("click", pushScore);
+    submit.onclick = pushScore;
   }
 
 }
@@ -77,32 +64,41 @@ function checkScores(){
 // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
 // https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
 
-// ↑↑↑ https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68=
+/* generates random number between 1 and 4 and pushes it to gameChoice array */
 
-function randomSequence(){ // generates random number between 1 and 4 and pushes it to gameChoice array
+function randomSequence(){
   gameChoice.push(Math.floor((Math.random() * 4) + 1));
 }
 
-function animate(button){ // created for consistent code and animaation, used specifically for user clicks of simon buttons
+/* created for consistent code and animaation, used specifically for user clicks of simon buttons */
+
+function animate(button){
   setTimeout(() => {
     button.classList.add("clicked");
     setTimeout(() => {button.classList.remove("clicked");}, 200);
   }, 100);
 }
 
+/* begins the game */
+
 function startClicked(){
   animate(start);
   startGame();
 }
 
+/* refreshes the page if the reset button is clicked */
+
 function resetClicked(){
   animate(reset);
   window.location.reload();
+  if (event.keyCode == 13) {
+  event.preventDefault();
+}
 }
 
-/*to describe all functions*/
+/* ends the game, activated if user makes an incorrect choice; empties both userChoice and gameChoice arrays */
 
-function gameOver(){ // ends the game, activated if user makes an incorrect choice; empties both userChoice and gameChoice arrays
+function gameOver(){
   gameOverMsg.classList.remove("hidden");
   one.classList.add("hidden");
   two.classList.add("hidden");
@@ -116,25 +112,33 @@ function gameOver(){ // ends the game, activated if user makes an incorrect choi
   setTimeout(checkScores, 1000);
 }
 
-function testChoice(){ // tests array equality by iteration
+/* tests array equality by iteration */
+
+function testChoice(){
+
+      /* while userChoice is less than the gameChoice length; if choices are equivalent, the game continues, if they are not, gameOver */
 
       if(userChoice.length < gameChoice.length){
-      for (i=0;i<userChoice.length;i++){
-        if(userChoice[i] === gameChoice[i]){
-            console.log("win");
-        }
-        else if(userChoice[i] !== gameChoice[i]){
-          console.log("<game over");
-          gameOver();
+        for (i=0;i<userChoice.length;i++){
+          if(userChoice[i] === gameChoice[i]){
+              console.log("win");
+          }
+          else if(userChoice[i] !== gameChoice[i]){
+            console.log("<game over");
+            gameOver();
+          }
         }
       }
-      }
+
+      /* if arrays are equal in length, test for equality; inequality, gameOver, equality, playGameChoice
+      user controls are deactivated by removing the event listener for the simon buttons */
 
       else if(userChoice.length === gameChoice.length){
         if(userChoice[userChoice.length - 1] !== gameChoice[gameChoice.length -1]){
             console.log("=game over");
             gameOver();
           }
+
         else if(userChoice[userChoice.length - 1] === gameChoice[gameChoice.length -1]){
             console.log("next round");
             userChoice = [];
@@ -146,34 +150,11 @@ function testChoice(){ // tests array equality by iteration
             setTimeout(playGameChoice, 2000);
           }
       }
-
-
-
-  // for (i=0;i<userChoice.length;i++){
-  //     if(userChoice.length < gameChoice.length){
-  //       if(userChoice[i] === gameChoice[i]){
-  //           console.log("win");
-  //       }
-  //       else if(userChoice[i] !== gameChoice[i]){
-  //         console.log("<game over");
-  //         gameOver();
-  //       }
-  //     }
-  //     else if(userChoice.length >= gameChoice.length){
-  //       if(userChoice[i] !== gameChoice[i]){
-  //           console.log("=game over");
-  //           gameOver();
-  //         }
-  //       else if(userChoice[i] === gameChoice[i]){
-  //           console.log("next round");
-  //           userChoice = [];
-  //           level++;
-  //           setTimeout(playGameChoice, 2000);
-  //         }
-  //     }
-  // }
 }
 
+/* while the userChoice is equivalent to the gameChoice, waitUserchoice is activate
+after each userChoice, the choice is tested against the gameChoice array
+if at any point the userChoice is incorrect, testChoice calls gameOver */
 
 function waitUserChoice(){
 
@@ -211,11 +192,6 @@ function waitUserChoice(){
       btn4Sound.play();
       testChoice();
     }
-    // eventlistener fires twice and causes code to fail
-    // one.addEventListener("click", clickOne);
-    // two.addEventListener("click", clickTwo);
-    // three.addEventListener("click", clickThree);
-    // four.addEventListener("click", clickFour);
 
     one.onclick = clickOne;
     two.onclick = clickTwo;
@@ -224,65 +200,63 @@ function waitUserChoice(){
 
   }
 
-function playGameChoice(){ // plays the games turn, animations in place for each gameChoice made, calls waitUserChoice at the end
+/* plays the games turn, animations in place for each gameChoice made, calls waitUserChoice at the end */
+
+function playGameChoice(){
 
   randomSequence();
   levelNumber.innerHTML = level;
   userTurn.classList.add("hidden");
   computerTurn.classList.remove("hidden");
 
-setTimeout(() => {
-  for (i=0; i<gameChoice.length; i++){
+  /* while i is less than gameChoice.length, the game switches on the button values 1-4, animating and playing sound accordingly */
 
-      switch(gameChoice[i]){
+  setTimeout(() => {
+    for (i=0; i<gameChoice.length; i++){
 
-        case 1:
-        setTimeout(() => {
-          one.classList.add("clicked");
-          btn1Sound.play();
-          setTimeout(() => {one.classList.remove("clicked");}, 500);
-        }, i*1000);
-        break;
+        switch(gameChoice[i]){
 
-        case 2:
-        setTimeout(() => {
-          two.classList.add("clicked");
-          btn2Sound.play();
-          setTimeout(() => {two.classList.remove("clicked");}, 500);
-        }, i*1000);
-        break;
+          case 1:
+          setTimeout(() => {
+            one.classList.add("clicked");
+            btn1Sound.play();
+            setTimeout(() => {one.classList.remove("clicked");}, 500);
+          }, i*1000);
+          break;
 
-        case 3:
-        setTimeout(() => {
-          three.classList.add("clicked");
-          btn3Sound.play();
-          setTimeout(() => {three.classList.remove("clicked");}, 500);
-        }, i*1000);
-        break;
+          case 2:
+          setTimeout(() => {
+            two.classList.add("clicked");
+            btn2Sound.play();
+            setTimeout(() => {two.classList.remove("clicked");}, 500);
+          }, i*1000);
+          break;
 
-        case 4:
-        setTimeout(() => {
-          four.classList.add("clicked");
-          btn4Sound.play();
-          setTimeout(() => {four.classList.remove("clicked");}, 500);
-        }, i*1000);
-        break;
+          case 3:
+          setTimeout(() => {
+            three.classList.add("clicked");
+            btn3Sound.play();
+            setTimeout(() => {three.classList.remove("clicked");}, 500);
+          }, i*1000);
+          break;
 
-      }
+          case 4:
+          setTimeout(() => {
+            four.classList.add("clicked");
+            btn4Sound.play();
+            setTimeout(() => {four.classList.remove("clicked");}, 500);
+          }, i*1000);
+          break;
 
-
-  }
-  setTimeout(() => {waitUserChoice();}, i * 1000);
-}, 500);
-
-  // setTimeout(() => {waitUserChoice()}, 1500);
-
-  // waitUserChoice();
-
+        }
+    }
+    setTimeout(() => {waitUserChoice();}, i * 1000);
+  }, 500);
 }
 
+/* tied to start.onclick, initiates playGameChoice() after 1 second */
 
-function startGame(){ // tied to start.onclick, initiates playGameChoice() after 1 second
+function startGame(){
   start.classList.add("hidden");
   computerTurn.classList.remove("hidden");
   levelHeader.classList.remove("hidden");
@@ -294,3 +268,12 @@ function startGame(){ // tied to start.onclick, initiates playGameChoice() after
 
 reset.onclick = resetClicked;
 start.onclick = startClicked;
+
+/* prevents keys from triggering buttons or subitting forms */
+
+document.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+        e.preventDefault();
+        return false;
+    }
+});
