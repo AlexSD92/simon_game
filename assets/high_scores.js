9f3e4644b732afde28d725495d0e65fd7ba9810a
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-01-09 07:59:50 +0000

    fonts made more responsive

--- assets/high_scores.js
+++ assets/high_scores.js
@@ -5,5 +5,5 @@ const highScores = JSON.parse(localStorage.getItem("scoreList")) ?? [];
 const highScoreList = document.getElementById("highscores");
 
 highScoreList.innerHTML = highScores
-  .map((scoreList) => `<li>${scoreList.userName} - - - ${scoreList.level}`)
+  .map((scoreList) => `<li>${scoreList.userName} - ${scoreList.level}`)
   .join('');
